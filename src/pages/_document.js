3f03b1225cd9c4ac4de51004c9b25b0473cb6010
psico-mulciber <158625834+Mulc1b3R@users.noreply.github.com>
2024-03-20 14:14:00 +0000

    Update _document.js
    changes made to document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,11 +5,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Page Title */}
-        <title>Open-Audio TTS</title>
+        
+        
         
         {/* Favicon */}
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
+        <link rel="icon" type="image/ico" sizes="16x16" href="/favicon.ico" />
         
         {/* Meta Tags for SEO */}
         <meta name="description" content="An OpenAI-powered Text-to-Speech tool." />
@@ -17,20 +17,25 @@ export default function Document() {
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.openaudio.ai/" />
-        <meta property="og:title" content="Open-Audio TTS" />
+        <meta property="og:title" content="Zendog EVE" />
         <meta property="og:description" content="An OpenAI-powered Text-to-Speech tool." />
         <meta property="og:image" content="https://www.openaudio.ai/og-image.svg" />
 
         {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://www.openaudio.ai/" />
-        <meta property="twitter:title" content="Open-Audio TTS" />
+        <meta property="twitter:title" content="Zendog EVE" />
         <meta property="twitter:description" content="An OpenAI-powered Text-to-Speech tool." />
         <meta property="twitter:image" content="https://www.openaudio.ai/og-image.svg" />
 
         {/* Add additional meta tags as needed */}
+
+
       </Head>
       <body>
+        <br>
+        </br><center>Text 2 mp3</center>
+        
         <Main />
         <NextScript />
         <Analytics />
